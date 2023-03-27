d716747875ad76cbc2efae1a185cb3d03b7739f4
seunghaen <tmdgus9701@naver.com>
2023-03-27 16:51:26 +0900

    api setting

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,13 @@
 import { Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { sample } from "../../api";
 import PageGrid from "../../UI/PageGrid";
 
 function LoginPage() {
   const navigate = useNavigate();
   const onLogin: React.FormEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault();
+    sample();
   };
   return (
     <PageGrid>
@@ -15,7 +17,7 @@ function LoginPage() {
         <button onSubmit={onLogin}>로그인</button>
         <button
           onClick={() => {
-            navigate("/signin");
+            sample();
           }}
         >
           회원가입
